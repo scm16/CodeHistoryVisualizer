426f10cfb548e67a15653505a3bcdc30b76a0467
somkk13@vt.edu <somkk13@vt.edu>
2016-04-25 18:10:48 -0400

    new test

--- webapp/tests.js
+++ webapp/tests.js
@@ -1,10 +1,24 @@
+
+define([
+    'app/main'
+    
+    ],
+    function(main) {
+    	QUnit.test( "Invalid Username Test", function( test1 ) {
+    	
+  		test1.ok( 1 == "1", "Passed!" );
+	});
+        
+    }
+);
+
+
+
 // Test for error message upon entering invalid GitHub username
-QUnit.test( "Invalid Username Test", function( test1 ) {
-  test1.ok( 1 == "1", "Passed!" );
-});
+
 
 // Test for retrieval of all public repositories upon entering valid
 // GitHub username
-QUnit.test( "Valid", function( test2 ) {
-  test2.ok( 2 == "2", "Passed")
-});
+// QUnit.test( "Valid", function( test2 ) {
+//   test2.ok( 2 == "2", "Passed")
+// });
