4a1c22d75c0f29e2805532848a9e1b6691e1961a
Steffen Moseley <Steffen Moseley>
2016-04-25 17:11:01 -0400

    Updated tests

--- webapp/tests.js
+++ webapp/tests.js
@@ -1,7 +1,10 @@
-QUnit.test( "hello test", function( require ) {
-  require.ok( 1 == "1", "Passed!" );
-  
+// Test for error message upon entering invalid GitHub username
+QUnit.test( "Invalid Username Test", function( test1 ) {
+  test1.ok( 1 == "1", "Passed!" );
 });
-QUnit.test( "SearchViewModel Test", function( SearchViewModel ) {
-  SearchViewModel.ok( 2 == "2", "Passed")
+
+// Test for retrieval of all public repositories upon entering valid
+// GitHub username
+QUnit.test( "Valid", function( test2 ) {
+  test2.ok( 2 == "2", "Passed")
 });
