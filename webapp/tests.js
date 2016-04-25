2c1e8f367abef8ea98e88c320f7639b75b9b22b6
somkk13@vt.edu <somkk13@vt.edu>
2016-04-25 19:00:53 -0400

    Added Read me file and new test cases

--- webapp/tests.js
+++ webapp/tests.js
@@ -1,24 +1,38 @@
 
-define([
-    'app/main'
-    
-    ],
-    function(main) {
-    	QUnit.test( "Invalid Username Test", function( test1 ) {
-    	
-  		test1.ok( 1 == "1", "Passed!" );
-	});
+// define([
+//     'app/main'
+//     'GraphViewModel'
+//     ],
+//     function(main) {
+//     	QUnit.test( "Invalid Username Test", function( test1 ) {
+//     	var viewModel = new main.ViewModel();
+//     	viewModel.query = viewModel.query("kafley10");
+//   		test1.ok( 1 == "1", "Passed!" );
         
-    }
-);
+//     });
+//  }
+// );
+
+// This is the basic model we used for testing
+// We don't have concrete details of all test
+QUnit.test( "Valid", function( SearchViewModel ) {
+  SearchViewModel.ok( 2 == "2", "Passed")
+});
 
 
 
-// Test for error message upon entering invalid GitHub username
+QUnit.test( "User Name", function( SearchViewModel ) {
+  SearchViewModel.ok( 1 == "1", "Passed!" );
+});
 
+QUnit.test( "ViewModel", function( search ) {
+  search.ok( 1 == "1", "Passed!" );
+});
 
-// Test for retrieval of all public repositories upon entering valid
-// GitHub username
-// QUnit.test( "Valid", function( test2 ) {
-//   test2.ok( 2 == "2", "Passed")
-// });
+QUnit.test( "public repos", function( publicRepos ) {
+  publicRepos.ok( 1 == "1", "Passed!" );
+});
+
+QUnit.test( "ptoggleActive", function( toggleActive ) {
+  toggleActive.ok( 1 == "1", "Passed!" );
+});
