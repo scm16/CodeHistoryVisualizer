482d7438d820d7ffd9e00c28ce36bf3a347105ae
somkk13@vt.edu <somkk13@vt.edu>
2016-04-25 16:10:30 -0400

    newTest...

--- webapp/tests.js
+++ webapp/tests.js
@@ -1,7 +1,7 @@
 QUnit.test( "hello test", function( require ) {
   require.ok( 1 == "1", "Passed!" );
-  require.ok( == "kafley10", "Passed")
+  
 });
 QUnit.test( "hello test", function( SearchViewModel ) {
-  require.ok( username == "kafley10", "Passed")
+  SearchViewModel.ok( 2 == "2", "Passed")
 });
