1d0eb5ebf2741ab79ade39d4870ffee3400023cf
Trambelus <trambelus2@gmail.com>
2016-04-24 21:38:35 -0400

    Began ko development

--- webapp/app/main.js
+++ webapp/app/main.js
@@ -1,10 +1,20 @@
 // Wrap app in define function, for require.js
 define(function (require) {
 	// load dependencies
-	var $ = require('jquery-2.2.3.min'),
-		ko = require('knockout-min'),
-		highcharts = require('highcharts');
+	var $ = require('../lib/jquery-2.2.3.min'),
+		ko = require('../lib/knockout-min'),
+		highcharts = require('../lib/highcharts');
 
 	// Main logic goes here
+	console.log("Loaded")
 
+	var SearchViewModel = function() {
+		this.query = ko.observable("");
+		this.repo_json = ko.observable("");
+		this.search = function() {
+			this.repo_json(JSON.stringify(jQuery.get("https://api.github.com/repos/" + this.query() + "/commits")));
+		};
+	};
+
+	ko.applyBindings(new SearchViewModel());
 });
