2c4af249c5677eefa9a722af501780313e69a4aa
somkk13@vt.edu <somkk13@vt.edu>
2016-04-25 16:05:33 -0400

    new unit test

--- webapp/tests.js
+++ webapp/tests.js
@@ -1,3 +1,7 @@
-QUnit.test( "hello test", function( assert ) {
-  assert.ok( 1 == "1", "Passed!" );
+QUnit.test( "hello test", function( require ) {
+  require.ok( 1 == "1", "Passed!" );
+  require.ok( == "kafley10", "Passed")
+});
+QUnit.test( "hello test", function( SearchViewModel ) {
+  require.ok( username == "kafley10", "Passed")
 });
